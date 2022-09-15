e5954154431a93e99e439092bdbc3b75ff5ea90d
Nanu <danyalsmail@gmail.com>
2022-09-15 13:22:09 -0400

    COLOR SUPPORT
    - Added color support for lines
    - Created interpolation values to make colors flow from start to
    finish
    - Added padding to space in order to keep form away from edges
    - Upscaled image to 512x512

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ArtGen
-Uses Python to create random generative art a la Art Blocks 
+####Uses Python to create random generative art a la Art Blocks 
+
+I apologize for the frequencies of the commits - as this is meant to be a portfolio piece, I'm trying to demonstrate the process by which this was built
+
+###DEPENDENCIES:
+- Pillow
+
+### TO-DO
+- Include coolors.co compatibility to generate pleasing palettes for the shape
--- generator.py
+++ generator.py
@@ -1,39 +1,63 @@
 from PIL import Image, ImageDraw
 import random
+# # Potentially manage colors with Coolors.co?
+# from coolors import coolor
+
+def random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def color_interpolation(start, end, factor:float):
+    reciprocal = 1 - factor
+    return (
+        int(start[0] * reciprocal + end[0] * factor),
+        int(start[1] * reciprocal + end[1] * factor),
+        int(start[2] * reciprocal + end[2] * factor)
+    )
 
 def generate_art():
     print("Working...")
     
     # Generate base image
-    image_size_px = 128
+    image_size_px = 512
     image_bg_color = (255, 255, 255)
+    padding_pct = 0.2
+    start_color = random_color()
+    end_color = random_color()
     image = Image.new(
         "RGB", 
         (image_size_px, image_size_px), 
         image_bg_color
     )
     
-    # Draw lines over base image
+    # Draw lines 
     draw = ImageDraw.Draw(image)
     
     # Create a list of each random points
     endpoints = []
     for _ in range(10):
         random_point = (
-            random.randint(0, image_size_px),
-            random.randint(0, image_size_px)
+            random.randint(int(padding_pct * image_size_px), image_size_px - int(image_size_px * padding_pct)),
+            random.randint(int(padding_pct * image_size_px), image_size_px - int(image_size_px * padding_pct))
         )
         endpoints.append(random_point)
-    # Create random lines using previous line's endpoint as subsequent start point
+    # Connects each point with its following point
+    line_thickness = 1
+    point_count = len(endpoints)- 1
     for i, point in enumerate(endpoints):
         start = point
-        if i == len(endpoints) - 1:
+        
+        # Checks if starting point is at the end of the list; if so, connects to first point
+        if i == point_count:
             end = endpoints[0]
         else:
             end = endpoints[i + 1]
+        
+        # Draws line
         line_xy = (start, end)
-        line_color = (0, 0, 0)
-        draw.line(line_xy, fill = line_color)
+        color_factor = i / point_count
+        line_color = color_interpolation(start_color, end_color, color_factor)
+        line_thickness += 1
+        draw.line(line_xy, fill = line_color, width = line_thickness)
     
     image.save("test-image.png")
 
