d496d8dc5b49c21ced299d27261982dcc936ab0d
Nanu <danyalsmail@gmail.com>
2022-09-15 15:30:46 -0400

    COLOR OPTIMIZATION
    - Initial color generation trascribed from HSV to RGB in order to
    ensure more consistently saturated colors
    - Individual lines drawn to unique overlay, then added using
    ImageChops to create more pleasing overlaps in lines

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # ArtGen
-####Uses Python to create random generative art a la Art Blocks 
+##Uses Python to create random generative art a la Art Blocks 
 
 I apologize for the frequencies of the commits - as this is meant to be a portfolio piece, I'm trying to demonstrate the process by which this was built
 
-###DEPENDENCIES:
+##DEPENDENCIES:
 - Pillow
 
+##USAGE:
+#Variables
+- **image_size_px** (line 29): adjusts the overall size of the canvas, in pixels
+- **padding_pct** (line 30): adjusts the margin as a percentage of the total canvas size
+- **line_count** (line 31): adjusts the total number of lines used in the piece
+- **line_thickness** (line 75): adjusts integer weight of first line; will need to be adjusted as canvas size is changed
+- **instance_count** (line 102): adjusts the number of images generated in the Images folder
+
 ### TO-DO
-- More consistently center images on background image
-- Create random background colors *while maintaining sufficient contrast*
 - Create better blending for color gradient
-- Overlay lines with pixels behind to make overlaps more pleasing
-- Export images as set with defined count
+- Anti-aliasing for smaller canvas sizes
+- Experiment with different shapes (arcs, polygons, ellipses)
 - Include coolors.co compatibility to generate pleasing palettes for the shape
--- generator.py
+++ generator.py
@@ -1,10 +1,20 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageChops
 import random
+import colorsys
 # # Potentially manage colors with Coolors.co?
 # from coolors import coolor
 
 def random_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    # Creates random hue for HSV color (keeps saturation of colors)
+    h = random.random()
+    s = 1
+    v = 1
+    
+    # Convert HSV to RGB
+    rgb_float = colorsys.hsv_to_rgb(h,s,v)
+    rgb = [int(i *255) for i in rgb_float]
+    
+    return tuple(rgb)
 
 def color_interpolation(start, end, factor:float):
     reciprocal = 1 - factor
@@ -14,13 +24,12 @@ def color_interpolation(start, end, factor:float):
         int(start[2] * reciprocal + end[2] * factor)
     )
 
-def generate_art():
-    print("Working...")
-    
+def generate_art(path: str):    
     # Generate base image
     image_size_px = 512
-    image_bg_color = (255, 255, 255)
     padding_pct = 0.2
+    line_count = 10
+    image_bg_color = (0, 0, 0)
     start_color = random_color()
     end_color = random_color()
     image = Image.new(
@@ -34,13 +43,14 @@ def generate_art():
     
     # Create a list of each random points
     endpoints = []
-    for _ in range(10):
+    for _ in range(line_count):
         random_point = (
             random.randint(int(padding_pct * image_size_px), image_size_px - int(image_size_px * padding_pct)),
             random.randint(int(padding_pct * image_size_px), image_size_px - int(image_size_px * padding_pct))
         )
         endpoints.append(random_point)
-        
+
+#Centering of image 
     # Define and #(draw bounding box for all points)
     min_x = min([p[0] for p in endpoints])
     max_x = max([p[0] for p in endpoints])
@@ -65,6 +75,11 @@ def generate_art():
     line_thickness = 1
     point_count = len(endpoints)- 1
     for i, point in enumerate(endpoints):
+        
+        # Create canvas overlay
+        overlay_image = Image.new("RGB", (image_size_px, image_size_px), image_bg_color)
+        overlay_draw = ImageDraw.Draw(overlay_image)
+        
         start = point
         
         # Checks if starting point is at the end of the list; if so, connects to first point
@@ -78,10 +93,15 @@ def generate_art():
         color_factor = i / point_count
         line_color = color_interpolation(start_color, end_color, color_factor)
         line_thickness += 2
-        draw.line(line_xy, fill = line_color, width = line_thickness)
-
-    image.save("test-image.png")
+        overlay_draw.line(line_xy, fill = line_color, width = line_thickness)
+        image = ImageChops.add(image, overlay_image)
+        
+    image.save(path)
     print("Done!")
 
+instance_count = 10
+
 if __name__ == "__main__":
-    generate_art()
+    for i in range(instance_count):
+        print(f"Creating image {i + 1}...")
+        generate_art(f"Images/test-image-{i}.png")
